51933dea7f4b2c7d4b4d0ea8380b2cc538ddbe37
c.cgamboa <carlosgamboa15@gmail.com>
2023-06-27 21:55:26 -0500

    adding bean for

--- src/main/java/com/example/demo2/phonebook/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo2/phonebook/security/SecurityConfiguration.java
@@ -5,22 +5,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
-//https://spring.io/guides/gs/securing-web/
+    //https://spring.io/guides/gs/securing-web/
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                );
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(User.withUsername("charlie")
+                .password(passwordEncoder().encode("charlie"))
+                .roles("USER").build());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
